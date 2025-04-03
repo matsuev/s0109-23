12635acfcdd5bfb9ddeb138f00bef5d211b2e73c
Alex Matsuev <alex.matsuev@gmail.com>
2025-04-03 17:56:03 +0300

    Update

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,50 @@
 // Use WebSocket transport endpoint.
-const client = new Centrifuge('ws://127.0.0.1:8000/connection/websocket');
-
-// Allocate Subscription to a channel.
-const newsSub = client.newSubscription('news');
-
-// React on `news` channel real-time publications.
-newsSub.on('publication', function (ctx)
-{
-   console.log(ctx);
-});
-
-// Trigger subscribe process.
-newsSub.subscribe();
+const client = new Centrifuge('ws://10.82.143.170:8080/centrifugo/connection/websocket');
 
 // Trigger actual connection establishement.
 client.connect();
+
+const pos = document.getElementById("pos")
+const username = document.getElementById("username")
+const chat = document.getElementById("chat")
+const message = document.getElementById("message")
+const list = document.getElementById("list")
+
+const subBtn = document.getElementById("sub")
+const sendBtn = document.getElementById("send")
+
+subBtn.addEventListener("click", (ev) =>
+{
+   const chatSub = client.newSubscription(chat.value)
+   chatSub.on('publication', (ctx) =>
+   {
+      let line = "<p><b>" + ctx.data.from + ":</b> " + ctx.data.message + "</p>"
+      list.innerHTML += line
+   })
+   chatSub.subscribe()
+})
+
+sendBtn.addEventListener("click", (ev) =>
+{
+   client.publish(chat.value, {
+      from: username.value,
+      message: message.value
+   })
+
+   message.value = ""
+})
+
+document.addEventListener("mousemove", (ev) =>
+{
+   client.publish("mousepos", {
+      x: ev.clientX,
+      y: ev.clientY
+   })
+})
+
+const mouseSub = client.newSubscription("mousepos")
+mouseSub.on('publication', (ctx) =>
+{
+   pos.innerText = "X: " + ctx.data.x + ", Y: " + ctx.data.y
+})
+mouseSub.subscribe()
