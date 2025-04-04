acecdbd36ceec6ed573cbac0be1dfc20734d6641
Alex Matsuev <alex.matsuev@gmail.com>
2025-04-04 12:13:48 +0300

    Add api-gateway service

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,10 @@
 // Use WebSocket transport endpoint.
-const client = new Centrifuge('ws://10.82.143.170:8080/centrifugo/connection/websocket');
+const client = new Centrifuge('ws://127.0.0.1:8080/centrifugo/connection/websocket', {
+   data: {
+      username: "alex",
+      password: "qwerty"
+   }
+});
 
 // Trigger actual connection establishement.
 client.connect();
